b89e92f12faa2dfeed177807ef94eec25f398ab2
ShwetaYadav224 <shweta22y@gmail.com>
2026-01-08 20:19:40 +0530

    Initial commit: Portfolio with routing, theme toggle using Recoil

---
+++ src/routes/index.tsx
@@ -0,0 +1,36 @@
+import { lazy } from 'react'
+
+const Home = lazy(() => import('../Pages/Home'));
+const About = lazy(() => import('../Pages/About'));
+const Contact = lazy(() => import('../Pages/Contact'));
+const Skills = lazy(() => import('../Pages/Skills'));
+const Projects = lazy(() => import('../Pages/Projects'));
+
+
+export const routes = [
+  {
+    path: '/',
+    element: <Home />,
+    title: 'Home'
+  },
+  {
+    path: '/about',
+    element: <About />,
+    title: 'About'
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+    title: 'Contact'
+  },
+  {
+    path: '/skills',
+    element: <Skills />,
+    title: 'Skills'
+  },
+  {
+    path: '/projects',
+    element: <Projects />,
+    title: 'Projects'
+  }
+]
